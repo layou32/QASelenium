88c33d83a7f184627d0dae61256fd108498d0ebd
layou32 <layou32@gmail.com>
2018-08-31 07:39:06 -0600

    asd

--- LimelightQATest/src/pages/SearchResultsPage.java
+++ LimelightQATest/src/pages/SearchResultsPage.java
@@ -1,18 +1,43 @@
 package pages;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchResultsPage {
 	private static WebElement element = null;
+	private static List<WebElement> elements = null;
+	private static WebDriverWait wait = null;
 	
 	public static WebElement caseFilter(WebDriver driver) {
-		element = driver.findElement(By.xpath("//div[@id='leftNavContainer']/ul/ul/div/li/span/a/span"));
+		// element = driver.findElement(By.cssSelector("span.a-size-small.a-color-base"));
+		element = driver.findElement(By.partialLinkText("Cases"));
 		return element;
 	}
 	
-	public static WebElement priceFilter(WebDriver driver) {
-		element = driver.findElement(By.xpath("//div[@id='leftNavContainer']/ul[17]/div/li[2]/span/a/span"));
+	public static WebElement priceFilter(WebDriver driver) {		
+		element = driver.findElement(By.linkText("$50 to $100"));
+		return element;
+	}
+	
+	public static List<WebElement> firstFive(WebDriver driver) {		
+		elements = driver.findElements(By.cssSelector("li[id^='result_']"));		
+		return elements;
+	}
+	
+	public static WebElement titleProduct(WebElement elem) {
+		element = elem.findElement(By.cssSelector("h2.s-access-title"));
+		return element;
+	}
+	
+	public static WebElement priceProduct(WebElement elem) {		
+		return element;
+	}
+	
+	public static WebElement starsProduct(WebElement elem) {
 		return element;
 	}
 }
--- LimelightQATest/src/tests/amazonTest/AmazonTest.java
+++ LimelightQATest/src/tests/amazonTest/AmazonTest.java
@@ -3,7 +3,10 @@ package tests.amazonTest;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -20,6 +23,7 @@ import selenium.DriverSetup;
 public class AmazonTest {
 	
 	private static WebDriver driver = null;
+	private static WebElement element = null;	
 
 	@BeforeClass(alwaysRun = true)
 	public void setupClass()
@@ -53,14 +57,49 @@ public class AmazonTest {
 	
 	public static void main(String[] args) {		
 		driver = DriverSetup.setupDriver(DriverSetup.Browser.Chrome, "chromedriver");		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.amazon.com");
-		HomePage.inputSearch(driver).sendKeys("ipad air 2 case");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		HomePage.inputSearch(driver).sendKeys("ipad air 2 case");		
 		HomePage.buttonSearch(driver).click();
 		// No existen tienes que aplicar el dropdown
-		SearchResultsPage.caseFilter(driver).click();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		SearchResultsPage.caseFilter(driver).click();		
 		SearchResultsPage.priceFilter(driver).click();
+		
+		if (SearchResultsPage.firstFive(driver).size() > 0) {			
+			for (int i = 0; i < 5; i++) {				
+				String name = "";
+				String price = "";
+				String stars = "";
+				
+				element = SearchResultsPage.firstFive(driver).get(i);							
+				
+				name = "Name: " + SearchResultsPage.titleProduct(element).getAttribute("data-attribute");
+				
+				boolean breakIt = true;
+		        while (true) {
+			        breakIt = true;
+			        try {
+			        	price = "Price: " + element.findElement(By.cssSelector("span.sx-price-whole")).getText();
+			        } catch (Exception e) {
+			            if (e.getMessage().contains("element is not attached")) {
+			                breakIt = false;
+			            }
+			        }
+			        if (breakIt) {
+			            break;
+			        }
+			    }
+											
+				// name = "Name: " + element.findElement(By.cssSelector("h2.s-access-title")).getAttribute("data-attribute");
+				// price = "Price: " + element.findElement(By.cssSelector("span.sx-price.sx-price-large")).getText();				
+				// stars = "Star's: " + element.findElement(By.cssSelector("i.a-icon-star span")).getText(); 
+				
+				System.out.println(name);
+				System.out.println(price);
+				//System.out.println(stars);
+			}
+		} else {
+			System.out.println("Nada de nada");
+		}		
 	}
 }
